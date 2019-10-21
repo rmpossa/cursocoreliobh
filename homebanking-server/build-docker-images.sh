3e5e76635f83f0aa01f05578f7e051654b1708bb
Rodrigo Possa <rmpossa@gmail.com>
2019-10-21 23:58:51 +0000

    Finalizando conteinerização dos microsserviços

--- homebanking-server/build-docker-images.sh
+++ homebanking-server/build-docker-images.sh
@@ -3,8 +3,11 @@ docker build --tag=alpine-java:base --rm=true .
 docker build --file=Dockerfile.maven --tag=maven:base --rm=true .
 docker build --file=Dockerfile.app --tag=homebanking-server:latest --rm=true .
 # executar o comando abaixo na pasta do projeto coaf
-docker build --file=../coaf/Dockerfile.app --tag=coaf-server:latest --rm=true .
+cd ../coaf
+docker build --file=Dockerfile.app --tag=coaf-server:latest --rm=true .
 # executar o comando abaixo na pasta do projeto cartao de credito
-docker build --file=../cartao-credito/Dockerfile.app --tag=cartaocredito-server:latest --rm=true . 
+cd ../cartao-credito
+docker build --file=Dockerfile.app --tag=cartaocredito-server:latest --rm=true . 
 # executar o comando abaixo na pasta do projeto api-geteway
-docker build --file=../api-gateway/Dockerfile.app --tag=api-gateway-server:latest --rm=true .
+cd ../api-gateway
+docker build --file=Dockerfile.app --tag=api-gateway-server:latest --rm=true .
